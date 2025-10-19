19c28353d374286ace0c9e8f8bb2e0e3f3c93e26
Billy Roberts <williamrobertsjr@gmail.com>
2025-10-19 18:52:16 +0000

    Snapshot before life to staging overwrite

--- page-cart.php
+++ page-cart.php
@@ -26,6 +26,27 @@ foreach ( $cart_items as $key => &$item ) {
     $discounted_total += $item['discounted_subtotal'];
 }
 
+// Get raw table update time (likely in UTC)
+global $wpdb;
+
+// Get MySQL's last UPDATE_TIME for the rapid_quote table
+$raw_time = $wpdb->get_var("
+  SELECT UPDATE_TIME
+  FROM information_schema.tables
+  WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'rapid_quote'
+");
+
+if ($raw_time) {
+    // Treat it as server-local (America/Chicago)
+    $dt = new DateTime($raw_time, new DateTimeZone('America/Chicago'));
+
+    // Format however you want before sending to Twig
+    $context['rapid_quote_last_updated'] = $dt->format('m/d/Y \a\t g:i A');
+} else {
+    $context['rapid_quote_last_updated'] = 'Unknown';
+}
+
 $context['cart'] = $cart;
 $context['cart_items'] = $cart_items;
 $context['cart_discounted_total'] = $discounted_total;
